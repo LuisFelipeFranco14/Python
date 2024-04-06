294ff6e0e8641bd7867c3b4f13a97c93104cbee2
adaptussolution <adaptussolution@gmail.com>
2024-04-06 16:33:17 -0300

    Criando a parte do migration

--- Web/Django/Agenda/contact/admin.py
+++ Web/Django/Agenda/contact/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from contact import models
 
 # Register your models here.
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'id', 'first_name', 'last_name', 'phone',
+    ordering = 'id', #descrencete seria o -id
+    # list_filter = 'create_date',
+    search_fields = 'id', 'first_name', 'last_name',
+    list_per_page = 5
+    list_max_show_all = 10 #mostrar tudo 
+    list_editable = 'first_name', 'last_name', #editar as informações
+    list_display_links = 'id', 'phone', #cria link no campo para acessar o registro 
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = '-id', #descrencete seria o -id
