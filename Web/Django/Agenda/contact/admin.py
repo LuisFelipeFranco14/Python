7f66a649924efbb55f1ec4aac89ea6dd8e22e236
adaptussolution <adaptussolution@gmail.com>
2024-04-07 22:43:49 -0300

    Criando views Grid de informações

--- Web/Django/Agenda/contact/admin.py
+++ Web/Django/Agenda/contact/admin.py
@@ -4,13 +4,13 @@ from contact import models
 # Register your models here.
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'id', 'first_name', 'last_name', 'phone',
+    list_display = 'id', 'first_name', 'last_name', 'phone', 'show',
     ordering = 'id', #descrencete seria o -id
     # list_filter = 'create_date',
     search_fields = 'id', 'first_name', 'last_name',
     list_per_page = 5
     list_max_show_all = 10 #mostrar tudo 
-    list_editable = 'first_name', 'last_name', #editar as informações
+    list_editable = 'first_name', 'last_name', 'show', #editar as informações
     list_display_links = 'id', 'phone', #cria link no campo para acessar o registro 
 
 @admin.register(models.Category)
