cc7d08869a679af5605015dc196fd2de8b1bc4be
adaptussolution <adaptussolution@gmail.com>
2024-04-11 00:06:16 -0300

    Criando a parte do migration

---
+++ Web/Django/Ecommerce/produto/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from . import models
+
+class VariacaoInline(admin.TabularInline):
+    model = models.Variacao
+    extra = 1
+
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'descricao_curta',
+                    'get_preco_formatado', 'get_preco_promocional_formatado']
+    inlines = [
+        VariacaoInline
+    ]
+
+# Register your models here.
+admin.site.register(models.Produto)
+admin.site.register(models.Variacao)
