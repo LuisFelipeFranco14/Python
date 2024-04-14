c165a81c9fa0bc4fbda69b931e6327bda025b308
adaptussolution <adaptussolution@gmail.com>
2024-04-14 08:40:08 -0300

    Arrumando a proteção do banco de dados

--- Web/Django/Ecommerce/project/settings.py
+++ Web/Django/Ecommerce/project/settings.py
@@ -100,11 +100,9 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ecommerce',
-        'USER': 'root',
-        'PASSWORD': '123456',
-        'HOST': '127.0.0.1',
-        'PORT':  '3306',
+        'OPTIONS': {
+            'read_default_file': './path/to/my.cnf',
+        },
     }
 }
 # Database
