2e2c08b9ccb5c97c60cb01eedcb43bf8128917ba
adaptussolution <adaptussolution@gmail.com>
2024-04-14 08:28:51 -0300

    Mudando para Mysql

--- Web/Django/Ecommerce/project/settings.py
+++ Web/Django/Ecommerce/project/settings.py
@@ -89,13 +89,28 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ecommerce',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1',
+        'PORT':  '3306',
     }
 }
-
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+# pip install mysql-connector-python
+# pip install mysqlclient.
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
